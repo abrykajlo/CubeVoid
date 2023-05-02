d376406bf4b4b9bf34e0d51a822ba4f725524ca1
Adam Brykajlo <adam.brykajlo@gmail.com>
2023-05-02 07:44:45 -0700

    ImGui added with docking

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -10,8 +10,8 @@ main()
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         return -1;
     }
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
                         SDL_GL_CONTEXT_PROFILE_CORE);
 
@@ -36,7 +36,15 @@ main()
     }
 
     ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+    
     if (!ImGui_ImplSDL2_InitForOpenGL(window, context)) {
+        return -1;
+    }
+
+    if (!ImGui_ImplOpenGL3_Init()) {
         return -1;
     }
 
@@ -52,7 +60,10 @@ main()
         }
 
         ImGui_ImplSDL2_NewFrame();
+        ImGui_ImplOpenGL3_NewFrame();
         ImGui::NewFrame();
+
+        ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
 
         ImGui::Begin("Hello World");
         if (ImGui::Button("Quit")) {
@@ -60,8 +71,24 @@ main()
         }
         ImGui::End();
         ImGui::EndFrame();
+
+        ImGui::Render();
+        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+        glClearColor(0, 0, 0, 0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // render additional platform windows
+        SDL_Window* backup_current_window = SDL_GL_GetCurrentWindow();
+        SDL_GLContext backup_current_context = SDL_GL_GetCurrentContext();
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+        SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
+
+        SDL_GL_SwapWindow(window);
     }
 
+    ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplSDL2_Shutdown();
     SDL_GL_DeleteContext(context);
     SDL_DestroyWindow(window);
